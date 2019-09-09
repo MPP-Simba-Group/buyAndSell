9f76d8a4029177697caef1098f1a8873d5fac034
Mostafa Anwar <mostafa.anwar10@gmail.com>
2019-09-09 13:04:03 -0500

    project init config

---
+++ src/main/java/com/mpp/buyAndSell/BuyAndSellApplication.java
@@ -0,0 +1,13 @@
+package com.mpp.buyAndSell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyAndSellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyAndSellApplication.class, args);
+	}
+
+}
